630eddd6cd1703efe29da113cde86a199ea39b48
Hangsihak Sin <sihak.hs@gmail.com>
2022-04-09 21:50:59 -0400

    Add userPlay Function to allow user to enter one of the options

--- script.js
+++ script.js
@@ -12,7 +12,20 @@ function computerPlay() {
 
 const computerSelection = computerPlay(); // stored computerPlay() return value to a variable to be use in playRound() parameter
 
+// User Play
+function userPlay() {
+    const userData = prompt ('Rock, Paper, Scissor. Please enter one of the options:', 'Rock');
+    let editedUserData = userData.toLowerCase(); // Convert User's Input to lowercase 
+    if ((editedUserData !== 'rock') && 
+        (editedUserData !== 'paper') && 
+        (editedUserData !== 'scissors')) {
+            console.log('Invalid entry. Please enter one of the Options!'); 
+    } else {
+        return editedUserData;
+    }
+}
 
+const userSelection = userPlay(); // stored userPlay() return value to a variable to be use in playRound() parameter
 
 
 
