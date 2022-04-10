08b50e118a7c1b92c544c90f122034d542625125
Hangsihak Sin <sihak.hs@gmail.com>
2022-04-09 23:44:04 -0400

    Add game() to loop 5 times

--- script.js
+++ script.js
@@ -10,11 +10,12 @@ function computerPlay() {
     }
 }
 
-const computerSelection = computerPlay(); // stored computerPlay() return value to a variable to be use in playRound() parameter
+// const computerSelection = computerPlay(); // stored computerPlay() return value to a variable to be use in playRound() parameter
 
 // User Play
+
 function userPlay() {
-    const userData = prompt ('Rock, Paper, Scissor. Please enter one of the options:', 'Rock');
+    const userData = prompt ('Rock, Paper, Scissors. Please enter one of the options:', 'rock');
     let editedUserData = userData.toLowerCase(); // Convert User's Input to lowercase 
     if ((editedUserData !== 'rock') && 
         (editedUserData !== 'paper') && 
@@ -25,7 +26,7 @@ function userPlay() {
     }
 }
 
-const userSelection = userPlay(); // stored userPlay() return value to a variable to be use in playRound() parameter
+// const userSelection = userPlay(); 
 
 // Play a Single Round
 function playRound(userSelection, computerSelection) {
@@ -42,7 +43,17 @@ function playRound(userSelection, computerSelection) {
     }
 }
 
-console.log(playRound(userSelection, computerSelection)); // Invoke playRound()
+// Loop Game 5 Times
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let userSelection = userPlay(); // stored userPlay() return value to a variable to be use in playRound() parameter
+        console.log('User:', userSelection);
+        let computerSelection = computerPlay();
+        console.log('Computer:', computerSelection)
+        console.log(playRound(userSelection, computerSelection));// Invoke playRound() to display the result.
+    }
+}
 
+game(); // Invoke game() so that as soon as the page is loaded. It will start the game
 
 
